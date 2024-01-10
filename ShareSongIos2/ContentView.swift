30b7157dd472cf17b632a5d111b205f023191249
Valerian Backeberg <valerian@backebergs.de>
2024-01-10 12:57:27 +0100

    Share Sheet logic

--- ShareSongIos2/ContentView.swift
+++ ShareSongIos2/ContentView.swift
@@ -1,40 +1,35 @@
-//
-//  ContentView.swift
-//  ShareSongIos2
-//
-//  Created by user935487 on 1/4/24.
-//
-
 import SwiftUI
-import SwiftData
+import SwiftData	
 
 struct ContentView: View {
     @State private var responseText: String = ""
     @State private var showShareSheet = false
-    @State private var itemsToShare: [Any] = ["This is a test string"]
-
-    var body: some View {
-
-    }
+    
 
     var body: some View {
         VStack {
             Text(responseText)
                 .padding()
                 .border(Color.gray)
-                    Button("Share") {
-                        Task {
-                                await convertSong(originUrl: "https://deezer.page.link/go6SQsExZhcaw1NM7", targetService: "AppleMusic")
-                            }
-                        showShareSheet = true
-                    }
-                .sheet(isPresented: $showShareSheet) {
-                    ShareSheet(items: itemsToShare, isPresented: $showShareSheet)
-                }
+            }
+            .padding()
+            .onOpenURL { url in processUrl(url) }
+            .sheet(isPresented: $showShareSheet) {
+                ShareSheet(items: [responseText], isPresented: $showShareSheet)
         }
-        .padding()
     }
 
+    
+    func processUrl(_ url: URL) {
+        print(url)
+        print(url.absoluteString)
+        Task {
+            await convertSong(originUrl: "https://deezer.page.link/go6SQsExZhcaw1NM7", targetService: "AppleMusic")
+            showShareSheet = true
+        }
+    }
+
+    
     private func convertSong(originUrl: String, targetService: String) async {
         do {
             let conversionResponse = try await ShareSongApi.convert(originServiceUrl: originUrl, targetService: targetService)
--- ShareSongIos2/ShareSheet.swift
+++ ShareSongIos2/ShareSheet.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SwiftUI
 
 struct ShareSheet: UIViewControllerRepresentable {
     var items: [Any]
