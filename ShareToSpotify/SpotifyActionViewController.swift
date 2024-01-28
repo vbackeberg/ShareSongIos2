b5810f9fd8478858027e165181d46699e31c2e74
Valerian Backeberg <valerian@backebergs.de>
2024-01-28 18:29:26 +0100

    SpotifyActionViewController: remove obsolete imports

--- ShareToSpotify/SpotifyActionViewController.swift
+++ ShareToSpotify/SpotifyActionViewController.swift
@@ -1,10 +1,6 @@
-import MobileCoreServices
-import UIKit
-import UniformTypeIdentifiers
-
 class SpotifyActionViewController: BaseActionViewController {
     override var targetServiceName: String { "Spotify" }
-    
+
     @IBAction func done() {
         close()
     }
