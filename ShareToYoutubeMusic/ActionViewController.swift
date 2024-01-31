fd02bcda591f0beedc12e9574b20a587c4076101
Valerian Backeberg <valerian@backebergs.de>
2024-01-31 20:26:38 +0100

    Add extensions Tidal, Youtube Music. Set correct activation predicate for Apple Music.

---
+++ ShareToYoutubeMusic/ActionViewController.swift
@@ -0,0 +1,10 @@
+import MobileCoreServices
+import SwiftUI
+import UIKit
+import UniformTypeIdentifiers
+
+/// A UI Kit ViewController that houses a SwiftUI View
+class YoutubeMusicActionViewController: BaseActionViewController {
+    override var targetServiceName: String { "YoutubeMusic" }
+    override var targetServiceDisplayName: String { "Youtube Music" }
+}
