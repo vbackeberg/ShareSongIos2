84f4577c21c01d5d313c0f1782f999cde929ae80
Valerian Backeberg <valerian@backebergs.de>
2024-01-31 12:57:08 +0100

    LoadingAnimationView

--- Common/LoadingAnimation.swift
+++ Common/LoadingAnimation.swift
@@ -1,8 +1,29 @@
-//
-//  LoadingAnimation.swift
-//  ShareSongIos2
-//
-//  Created by Mewval on 28.01.24.
-//
-
 import Foundation
+import SwiftUI
+
+struct LoadingAnimation: View {
+    @State private var isAnimating = false
+
+    var body: some View {
+        HStack {
+            Circle()
+                .frame(width: 10, height: 10)
+                .scaleEffect(isAnimating ? 1 : 0.5)
+            Circle()
+                .frame(width: 10, height: 10)
+                .scaleEffect(isAnimating ? 1 : 0.5)
+            Circle()
+                .frame(width: 10, height: 10)
+                .scaleEffect(isAnimating ? 1 : 0.5)
+        }
+        .onAppear {
+            withAnimation(Animation.easeInOut(duration: 0.5).repeatForever()) {
+                isAnimating = true
+            }
+        }
+    }
+}
+
+#Preview {
+    LoadingAnimation()
+}
