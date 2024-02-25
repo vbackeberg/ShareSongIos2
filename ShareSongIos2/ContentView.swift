86595af29489a1ee218a09ceac0cd962120153a0
Valerian Backeberg <valerian@backebergs.de>
2024-02-25 21:56:25 +0100

    Create informal main view, set App icon, set display name

--- ShareSongIos2/ContentView.swift
+++ ShareSongIos2/ContentView.swift
@@ -5,12 +5,26 @@ struct ContentView: View {
     @State private var responseText: String = ""
 
     var body: some View {
-        VStack {
-            Text(responseText)
-                .padding()
-                .border(Color.gray)
+        VStack(spacing: 16) {
+            Text("Sharing songs with others made easy")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding(16)
+                .font(.system(size: 24))
+            Text("This app can convert links you share from music services to other services, e.g. from Spotify to Deezer. Next time you share a song, look out for the new sharing options.")
+                .padding(16)
+                .font(.system(size: 16))
+            Button(action: { exit(0) }) {
+                Text("Cool")
+                    .fontWeight(.bold)
+                    .font(.system(size: 32))
+                    .frame(width: 256, height: 64)
+                    .background(Color(UIColor.purple))
+                    .foregroundColor(Color(UIColor.white))
+                    .cornerRadius(16)
+            }
+            .padding(.top, 64)
         }
-        .padding()
     }
 }
 
