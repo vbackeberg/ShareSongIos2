e256867b2e08150f9a60493916b7c5ca79200d36
Valerian Backeberg <valerian@backebergs.de>
2024-09-08 18:45:35 +0200

    Remove all now obsoelete action view controllers since we only need the one that converts and opens. Move dependencies into actionviewcontroller package

--- ShareToDeezer/DeezerActionViewController.swift
+++
@@ -1,3 +0,0 @@
-class DeezerActionViewController: BaseActionViewController {
-    override var targetServiceName: String { "Deezer" }
-}
--- ShareToTidal/ActionViewController.swift
+++
@@ -1,9 +0,0 @@
-import MobileCoreServices
-import SwiftUI
-import UIKit
-import UniformTypeIdentifiers
-
-/// A UI Kit ViewController that houses a SwiftUI View
-class TidalActionViewController: BaseActionViewController {
-    override var targetServiceName: String { "Tidal" }
-}
