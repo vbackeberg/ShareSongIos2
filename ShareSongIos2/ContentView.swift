ebe3266bce52d2e296d3d3ffb94d19d2a747137b
Müge Güzet <mg@Muges-Laptop.fritz.box>
2024-09-08 08:45:55 +0200

    settings view

--- ShareSongIos2/ContentView.swift
+++ ShareSongIos2/ContentView.swift
@@ -1,68 +1,78 @@
-import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    @State private var responseText: String = ""
+    @State private var selectedService: String = UserDefaults.standard.string(forKey: "serviceOfUser") ?? "no"
     
-    @State private var originServiceUrl: String = ""
-    @State private var showConversion: = false
-    enum Service: String, CaseIterable, Identifiable {
-        case Spotify, AppleMusic, YoutubeMusic, Deezer, Tidal
-        var id: Self { self }
-    }
-    @State private var selectedService: Service = Service.AppleMusic
-
+    private let items: [String: String] = [
+        "Spotify": "Spotify",
+        "Deezer": "Deezer",
+        "Tidal": "Tidal",
+        "AppleMusic": "Apple Music",
+        "YoutubeMusic": "Youtube Music"
+    ]
+    
     var body: some View {
-        VStack(spacing: 16) {
-            Spacer()
-            Text("Sharing songs with others made easy 🎵")
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding()
-                .font(.system(size: 24))
-            Text("This app lets you convert links from one music service to another. Give it a try! Click the share button below and look for the options in the list.")
-                .padding()
-                .font(.system(size: 16))
-
-            TextField("Enter a song link", text: $originServiceUrl)
-            List {
-                Picker("Choose music service", selection: $selectedService) {
-                    Text("Spotify").tag(Service.Spotify)
-                    Text("Apple Music").tag(Service.AppleMusic)
-                    Text("Youtube Music").tag(Service.YoutubeMusic)
-                    Text("Deezer").tag(Service.Deezer)
-                    Text("Tidal").tag(Service.Tidal)
+        ScrollView {
+            VStack(alignment: .center, spacing: 16) {
+                Text("Select your music service")
+                    .font(.system(size: 24, weight: .bold))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                
+                Text("Choose the music service we should convert your song to. Next, select and share any song link and look for the Share Song icon. Tap it to convert your song!")
+                    .font(.system(size: 16))
+                    .multilineTextAlignment(.center)
+                    .padding(16)
+                
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
+                    ForEach(items.keys.sorted(), id: \.self) { key in
+                        ButtonWithOutline(
+                            text: items[key]!,
+                            isSelected: key == selectedService,
+                            onSelected: {
+                                selectedService = key
+                                saveSelectedService(service: key)
+                            }
+                        )
+                    }
                 }
+                .padding(.horizontal, 16)
             }
-            
-            Button() {
-                showConversion = true
-            }.sheet(isPresented: $showConversion) {
-                ConversionViewController(originServiceUrl, targetServiceName=selectedService.rawValue)
-            }
-            
-            Spacer()
+            .padding(.top, 48)
         }
+        .background(Color(uiColor: .systemBackground))
+        .edgesIgnoringSafeArea(.bottom)
     }
     
-    struct ConversionViewController: UIViewControllerRepresentable {
-        let originServiceUrl: String
-        let targetServiceName: String
-        
-        func makeUIViewController(context: Context) -> BaseActionViewController {
-            let baseActionViewController = BaseActionViewController()
-            baseActionViewController.targetServiceName = targetServiceName
-            baseActionViewController.
-            
-            return baseActionViewController
-        }
+    // Function to save selected service to UserDefaults
+    func saveSelectedService(service: String) {
+        UserDefaults.standard.set(service, forKey: "serviceOfUser")
+    }
+}
 
-        func updateUIViewController(_ uiViewController: BaseActionViewController, context: Context) {
-            code
+struct ButtonWithOutline: View {
+    let text: String
+    let isSelected: Bool
+    let onSelected: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            onSelected()
+        }) {
+            Text(text)
+                .font(.system(size: 20))
+                .frame(width: 128, height: 96)
+                .background(isSelected ? Color.blue : Color.gray.opacity(0.3))
+                .cornerRadius(16)
+                .foregroundColor(isSelected ? Color.white : Color.primary)
+                .padding(8)
+                .multilineTextAlignment(.center)
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
